08aba9be37ad4e81a4e92c6333310c879980c09c
yarden cohen <yarden95c@gmail.com>
2017-12-15 14:31:19 +0200

    Add project files.

---
+++ Project/Server/SpecificFact.cs
@@ -0,0 +1,19 @@
+﻿using Project.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Server
+{
+    class SpecificFact
+    {
+        private DbManager _dbManager;
+        public SpecificFact(DbManager dbManager)
+        {
+            _dbManager = dbManager;
+        }
+
+    }
+}
